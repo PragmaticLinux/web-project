8c756fa3ea027245e7d46e574fdf33ee721faec3
Alban Mulaki <amulaki@pragmaticlinux.org>
2016-09-28 19:40:41 +0200

    Add page get help with icon, structure #20

---
+++ http/wp-content/themes/pragmaticlinux/page-get-help.php
@@ -0,0 +1,95 @@
+<?php get_header(); ?>
+
+<div class="row header-page text-center">
+    <h1>Team</h1> 
+</div>
+<div class="row">
+    <div class="container">
+
+        <div class="col-md-12">
+            <br><br>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-3 text-center get-help" >
+                <a href="<?php echo home_url(); ?>/forums">
+                    <i class="fa fa-group fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Community Forum</h4>
+                    <p>
+                        Community,Pragmatic Linux, Forum, Documentation, Help others, ask your question
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="<?php echo home_url(); ?>/frequently-ask-and-question-faq/">
+                    <i class="fa fa-question-circle-o fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Pragmatic Linux FAQs</h4>
+                    <p>
+                        Quick question, check, frequently,
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="https://github.com/PragmaticLinux/PragmaticLinux-Server/issues">
+                    <i class="fa fa-file-code-o fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Report a Bug</h4>
+                    <p>
+                        Quick question, check, frequently,
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="<?php echo home_url(); ?>/video/01-how-to-install-pragmatic-linux-server-env/">
+                    <i class="fa fa-play-circle-o fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Video Walkthrough</h4>
+                    <p>
+                        Walkthrough, basic task, setting up, installing, Subscribe youtube channel,
+                    </p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <br><br>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-3 text-center get-help" >
+                <a href="<?php echo home_url(); ?>/wiki/index.php/Main_Page">
+                    <i class="fa fa-wikipedia-w fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Documentation Wiki</h4>
+                    <p>
+                        Get official documentation,manual,usage,how to, guides
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="#">
+                    <i class="fa fa-puzzle-piece fa-4x"></i><br><br>
+                    <h4 style="font-weight:bold;">Get Involved</h4>
+                    <p>
+                        getinvolved@pragmaticlinux.org, involved Collaborate,
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="#">
+                    <i class="fa fa-share-alt fa-4x"></i><br><br>
+                    <h4>Collaborate</h4>
+                    <p>
+                        info@pragmaticlinux.org, Collaborate with us, projects, Software Development, linux, sys admin,
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-3 text-center get-help" >
+                <a href="#">
+                    <i class="fa fa-calendar-check-o fa-4x"></i><br><br>
+                    <h4>Participate in events</h4>
+                    <p>
+                        Organize, participate, contribute, presentation
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br>
+<?php get_footer(); ?>
+
