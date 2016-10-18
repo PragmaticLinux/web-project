c4dc4fed19bf4864695394d9e7c95753fd43d396
Alban Mulaki <amulaki@pragmaticlinux.org>
2016-10-18 03:38:45 -0500

    Get Help content #20

--- http/wp-content/themes/pragmaticlinux/page-get-help.php
+++ http/wp-content/themes/pragmaticlinux/page-get-help.php
@@ -15,7 +15,8 @@
                     <i class="fa fa-group fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Community Forum</h4>
                     <p>
-                        Community,Pragmatic Linux, Forum, Documentation, Help others, ask your question
+The Pragmatic Linux community forum is dedicated for information exchange between users of Pragmatic Linux where users can help each other, ask 
+questions, respond to questions, share documentation, communicate with each other and share their experience with Pragmatic Linux. 
                     </p>
                 </a>
             </div>
@@ -24,7 +25,8 @@
                     <i class="fa fa-question-circle-o fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Pragmatic Linux FAQs</h4>
                     <p>
-                        Quick question, check, frequently,
+Here you find answers to the most frequent asked questions. Please first check these quick questions if you have any questions. If your question is 
+not listed, feel free to visit the community forum or contact us directly. 
                     </p>
                 </a>
             </div>
@@ -33,7 +35,8 @@
                     <i class="fa fa-file-code-o fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Report a Bug</h4>
                     <p>
-                        Quick question, check, frequently,
+In case you find a Bug in Pragmatic Linux, please report this bug to us. We are thankful and open for any suggestions which can improve the user 
+experience of Pragmatic Linux.
                     </p>
                 </a>
             </div>
@@ -42,7 +45,8 @@
                     <i class="fa fa-play-circle-o fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Video Walkthrough</h4>
                     <p>
-                        Walkthrough, basic task, setting up, installing, Subscribe youtube channel,
+Here you find video tutorials which show you how to walkthrough the installation process, setting up and basic tasks in Pragmatic Linux. You can also 
+subscribe to our official YouTube channel. 
                     </p>
                 </a>
             </div>
@@ -56,7 +60,9 @@
                     <i class="fa fa-wikipedia-w fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Documentation Wiki</h4>
                     <p>
-                        Get official documentation,manual,usage,how to, guides
+Here you find our official Pragmatic Linux documentation which includes the general user manual, different guides on how to install, set up and work 
+with Pragmatic Linux. This documentation is filled with rich and detailed content for every feature.
+
                     </p>
                 </a>
             </div>
@@ -65,7 +71,8 @@
                     <i class="fa fa-puzzle-piece fa-4x"></i><br><br>
                     <h4 style="font-weight:bold;">Get Involved</h4>
                     <p>
-                        getinvolved@pragmaticlinux.org, involved Collaborate,
+If you want to get involved with Pragmatic Linux and be a part of our team and community, please contact us at: <a 
+href="mailto:getinvolved@pragmaticlinux.org" >getinvolved@pragmaticlinux.org</a>
                     </p>
                 </a>
             </div>
@@ -74,7 +81,8 @@
                     <i class="fa fa-share-alt fa-4x"></i><br><br>
                     <h4>Collaborate</h4>
                     <p>
-                        info@pragmaticlinux.org, Collaborate with us, projects, Software Development, linux, sys admin,
+If you are interested in collaborating with us, by working in various projects together, be part of the Software Development team, or become a 
+Sysadmin, please contact us at: <a href="mailto:info@pragmaticlinux.org">info@pragmaticlinux.org</a> 
                     </p>
                 </a>
             </div>
@@ -83,7 +91,8 @@
                     <i class="fa fa-calendar-check-o fa-4x"></i><br><br>
                     <h4>Participate in events</h4>
                     <p>
-                        Organize, participate, contribute, presentation
+You can participate in our events by helping us organizing the event, by being physically or virtually present, contributing with any kind of 
+donation or giving a presentation at the event.
                     </p>
                 </a>
             </div>
